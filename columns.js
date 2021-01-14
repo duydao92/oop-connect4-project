b7cac95dc306acc0f31d605373b932d9ee5ed73a
duy <ddao0892@gmail.com>
2021-01-14 15:44:57 -0700

    column class created

--- columns.js
+++ columns.js
@@ -3,7 +3,23 @@
 
 export class Columns {
   consturctor(){
+    this.slots = Array(6).fill(null)
 
+  }
+
+  add(playerNum) {//store player number in bottom most entry available in column
+    let slot;
+    for (let i = slots.length; i >= 0; i--) {
+      if (slots[i] === null) {
+        slot === i;
+      }
+    }
+
+    slots[slot - 1] = playerNum
+  }
+
+  getTokenAt(row) {//return null if no token, 1 if black, 2 if red
+    return this.slots[row]
   }
 
 }
--- game.js
+++ game.js
@@ -1,17 +1,23 @@
 // SOLELY RESPONSIBLE FOR MANAGING THE STATE OF GAME
 
+import { Columns } from "./columns";
+
 export class Game {
   constructor (player1, player2){
     this.player1 = player1;
     this.player2 = player2;
     this.currentPlayer = 1;
+    this.columns = Array(7).fill(new Columns)
+  }
+  getTokenAt (row, col) { //return null if the square is empty, 1 if black is there, 2 if red.
+
   }
 
   getName(){ // return a string of player names
       return `${this.player1} VS. ${this.player2}`
   }
 
-  playInColumn(click){
+  playInColumn(col){
     if(this.currentPlayer === 1){
       this.currentPlayer = 2;
     } else {
